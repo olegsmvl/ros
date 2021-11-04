46b5d76ef4399298e465d8849f1a4b0b24695e94
olegsmvl2@mail.ru <olegsmvl2@mail.ru>
2021-11-04 19:04:54 +0300

    upd

--- test_rpi/app.cpp
+++ test_rpi/app.cpp
@@ -2,6 +2,12 @@
 #include <iostream>
 #include <wiringPi.h>
 
+extern "C"{
+	uint8_t read(){
+		return digitalRead(17);
+	}
+}
+
 int main(){
 	constexpr uint8_t ENCODER_1_PIN_A = 17;  // Wiring pi 0 = BCM 17
 	constexpr uint8_t ENCODER_1_PIN_B = 27;  // Wiring pi 2 = BCM 27
@@ -9,9 +15,10 @@ int main(){
 	constexpr uint8_t ENCODER_2_PIN_B = 25;  // Wiring pi 6 = BCM 25
 
 	while (true){
-		uint8_t val_A = digitalRead(17);		
+		uint8_t val_A = read();		
 		std::cout << "val A " << val_A  << std::endl;
 		
 	}	
 	return 0;
 }
+
--- test_rpi/make.sh
+++ test_rpi/make.sh
@@ -1,3 +1,4 @@
+rm -Rf build
 mkdir build
 g++ app.cpp -lwiringPi -o build/app
 ./app
