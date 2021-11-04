0469855d1d65857966fed01c312e6b0609d437a7
Ubuntu <olegsmvl2@mail.ru>
2021-11-04 12:25:23 +0000

    add gitignore

--- build/abot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/comp/ros/src/abot_description
-# Build directory: /home/comp/ros/build/abot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/comp/ros/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/comp/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/comp/catkin_ws/devel;/opt/ros/noetic' != '' else []
